d6866057869681365ba3c93d70cd760656fb24a6
Athulrajs <athul859062@gmail.com>
2026-06-13 15:08:32 +0530

    initial commit

---
+++ Website/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Page17 from './components/Page17'
+
+function App() {
+
+  return (
+    <>
+    <Page17/>
+    </>
+  )
+}
+
+export default App
