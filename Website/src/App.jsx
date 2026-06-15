171ed82d72d1bf246ca6ec74a86cdb0ebc0baf0e
Athulrajs <athul859062@gmail.com>
2026-06-15 21:07:21 +0530

    Initialize the app

--- Website/src/App.jsx
+++ Website/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Page17 from './components/Page17'
+import Page18 from './components/Page18'
+import Page19 from './components/Page19'
+import Page20 from './components/Page20'
 
 function App() {
 
   return (
-    <>
-    <Page17/>
-    </>
+    <div className='bg-[rgb(17,24,39)]'>
+      <Page17/>
+      <Page18/>
+      <Page19/>
+      <Page20/>
+    </div>
   )
 }
 
